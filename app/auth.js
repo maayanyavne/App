15532ecfb7fb5ab4c855e489d016d980062f3405
Maayan Yavne <maayan.yavne@gmail.com>
2014-07-27 22:04:51 -0700

    First Commit

---
+++ app/auth.js
@@ -0,0 +1,44 @@
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: 'your-secret-clientID-here', // your App ID
+		'clientSecret' 	: 'your-client-secret-here', // your App Secret
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'your-consumer-key-here',
+		'consumerSecret' 	: 'your-client-secret-here',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://128.12.160.33:8888/auth/google/callback'
+	},
+	'cleverAuthDev' : {
+		'clientID' 		: '87df15e166dd46d2d22d',
+		'clientSecret' 	: '15cf49bf34cc44d090054988f6031342f36116da',
+		'callbackURL' 	: encodeURIComponent('http://128.12.160.33:8888/auth/clever/callback'),
+		'district_id'	: '52c5e88553a943970d001718',
+		'scope'			: encodeURIComponent('read:user'),
+		'response_type'	: 'code'
+	},
+	'cleverAuthProd' : {
+		'clientID' 		: '94436bc012d738629cec',
+		'clientSecret' 	: 'fd33dcb16f3af4c7a30e925bb81d82a3be976db9',
+		'callbackURL' 	: encodeURIComponent('http://128.12.160.33:8888/auth/clever/callback'),
+		'district_id'	: '52c5e88553a943970d001718',
+		'scope'			: encodeURIComponent('read:user'),
+		'response_type'	: 'code'
+	},
+	'cleverAPI'		: {
+		'cleverOAuthInit'	: 'https://clever.com/oauth/authorize',
+		'cleverOAuthToken'	: 'https://clever.com/oauth/token'
+		
+	}
+};
---
+++ app/oAuth.js
@@ -0,0 +1,13 @@
+/**
+ * @author Maayan Yavne
+ */
+
+exports.startOAuth = function(req, res) {
+	//Get Clever signin data 
+	//Create path
+	//Send GET request
+	console.log('Starting OAuth');
+	
+};
+
+
