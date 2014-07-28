3530a0d321052f9432bf78bec953982ee70d18bb
Maayan Yavne <maayan.yavne@gmail.com>
2014-07-28 14:11:54 -0700

    Adding Database URL

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-	'url' : 'mongodb://localhost:27017/easy-node-authentication' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	'url' : 'mongodb://heroku_app27877583:81jkd6tun51f1nd9a822lcmm8s@ds053459.mongolab.com:53459/heroku_app27877583' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
 
 };
