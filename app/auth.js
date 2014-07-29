9c488e8f2c934c5aeeb2d640d5d39dc4b56da39d
Maayan Yavne <maayan.yavne@gmail.com>
2014-07-29 16:17:37 -0700

    Redo

--- app/auth.js
+++ app/auth.js
@@ -21,9 +21,18 @@ module.exports = {
 		'callbackURL' 	: 'http://128.12.160.33:8888/auth/google/callback'
 	},
 	'cleverAuthDev' : {
+		'clientID' 		: '87df15e166dd46d2d22d',
+		'clientSecret' 	: '15cf49bf34cc44d090054988f6031342f36116da',
+		'callbackURL' 	: encodeURIComponent('https://bell-schedule-app-maayanyavne.herokuapp.com/auth/clever/callback'),//encodeURIComponent('https://128.12.160.33:8080/auth/clever/callback'),
+		'district_id'	: '52c5e88553a943970d001718',
+		'scope'			: encodeURIComponent('read:user'),
+		'response_type'	: 'code',
+		'token'			: 'ed35413288c27d424ba726499d197630d80eb753'
+	},
+	'cleverAuthProd' : {
 		'clientID' 		: '94436bc012d738629cec',
 		'clientSecret' 	: 'fd33dcb16f3af4c7a30e925bb81d82a3be976db9',
-		'callbackURL' 	: encodeURIComponent('https://bell-schedule-app-maayanyavne.herokuapp.com/auth/clever/callback'),//encodeURIComponent('https://128.12.160.33:8080/auth/clever/callback'),
+		'callbackURL' 	: encodeURIComponent('https://bell-schedule-app-maayanyavne.herokuapp.com/auth/clever/callback'),
 		'district_id'	: '52c5e88553a943970d001718',
 		'scope'			: encodeURIComponent('read:user'),
 		'response_type'	: 'code',
